90553a0dd2e18e7dd4ba1efd02324300f16b4dce
MinHyungJung <74130989+MinHyungJung@users.noreply.github.com>
2020-12-01 09:51:08 +0900

    Create sketch.js
    Add. function learning

---
+++ sketch.js
@@ -0,0 +1,27 @@
+function learning(x, y, xvel, yvel){
+    this.pos = createVector(x,y);
+    this.vel = createVector(random(-1, 1), random(-2, 2));
+    this.lifespan = 10;
+  
+  
+    this.draw = function(){
+      stroke(200, this.lifespan);
+    strokeWeight(2);
+    fill(127, this.lifespan*10);
+       ellipse(this.pos.x,this.pos.y,12,12); 
+    }
+  
+    this.move = function(){
+      this.pos.add(this.vel);
+      this.vel.mult(friction);
+      this.vel.limit(maxSpeed);
+      this.lifespan++;
+    }
+  
+    this.magnet = function(){
+      var magpull = p5.Vector.sub(me,this.pos);
+      var magstrength = meStrength / this.pos.dist(me);
+      magpull.normalize().mult(magstrength);
+      this.vel.add(magpull);
+    }
+}
